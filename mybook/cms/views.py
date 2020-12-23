5c9a3c4f7cbbd03abd4f3a4c4b408e4ff0d5c6dc
YukiarC <yuukihagino@gmail.com>
2020-12-23 12:32:51 +0900

    modify edit.html

--- mybook/cms/views.py
+++ mybook/cms/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from cms.models import Book
-
+from cms.forms import BookForm
 
 def book_list(request):
     """書籍の一覧"""
@@ -15,7 +15,22 @@ def book_list(request):
 
 def book_edit(request, book_id=None):
     """書籍の編集"""
-    return HttpResponse('書籍の編集')
+    # return HttpResponse('書籍の編集')
+    if book_id:   # book_id が指定されている (修正時)
+        book = get_object_or_404(Book, pk=book_id)
+    else:         # book_id が指定されていない (追加時)
+        book = Book()
+
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)  # POST された request データからフォームを作成
+        if form.is_valid():    # フォームのバリデーション
+            book = form.save(commit=False)
+            book.save()
+            return redirect('cms:book_list')
+    else:    # GET の時
+        form = BookForm(instance=book)  # book インスタンスからフォームを作成
+
+    return render(request, 'cms/book_edit.html', dict(form=form, book_id=book_id))
 
 
 def book_del(request, book_id):
