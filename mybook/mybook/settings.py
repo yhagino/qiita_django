c11604f03d1a2d9ea870311de44e04d4030e65b4
YukiarC <yuukihagino@gmail.com>
2020-12-23 11:45:05 +0900

    added bootstrap4

--- mybook/mybook/settings.py
+++ mybook/mybook/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cms.apps.CmsConfig',   # cms アプリケーション
+    'bootstrap4',           # django-bootstrap4
 ]
 
 MIDDLEWARE = [
